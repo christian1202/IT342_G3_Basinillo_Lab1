6de4c441330a75fbe4e92d9be6090fde4a93fdfe
LAB13_USER ON WS08 <L13Y12W08@LabHCI.net>
2026-02-07 13:15:15 +0800

    backend setup

---
+++ backend/basinillo/src/main/java/com/it342/basinillo/BasinilloApplication.java
@@ -0,0 +1,13 @@
+package com.it342.basinillo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasinilloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasinilloApplication.class, args);
+	}
+
+}
